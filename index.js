0b0debc44c2d5181a1a46a3d34f01b30c601e4c2
Heidi <heidi@apartmentlist.com>
2016-05-01 17:16:52 +1200

    working on OAuth

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require("express")
 var path = require("path")
 
@@ -7,13 +8,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.listen(3000, function(){
+console.log("We have lift off! @ 3000")
+})
 
 app.get('/', function(req, res){
+
   res.render('index')
 })
 
 
-app.listen(3000, function(){
-console.log("We have lift off! @ 3000")
-})
 
